9005a993d8d49f1c378ad39d23a0b6fc22b88250
awberle <axelberle@yahoo.de>
2021-09-26 15:51:32 +0200

    reporting at cucumber.io
    - removed key from file

--- src/test/java/pizzaria/cucumber/features/RunCucumberTest.java
+++ src/test/java/pizzaria/cucumber/features/RunCucumberTest.java
@@ -1,11 +1,10 @@
-package website.loyaltypoints.cucumber.features;
+package pizzaria.cucumber.features;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/pizzaria/cucumber/features", glue = "pizzaria.cucumber")
+@CucumberOptions(features = "src/test/java/pizzaria/cucumber/features", glue = "pizzaria.cucumber", publish = true)
 public class RunCucumberTest {
-
 }
