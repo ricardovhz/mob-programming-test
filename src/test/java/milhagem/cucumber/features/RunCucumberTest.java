a606b5c72b6ca6853837f03f2781cd6322bde93f
Ricardo Vanhoz Ribeiro <ricardo@idtrust.com.br>
2021-09-28 16:11:10 -0300

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/java/milhagem/cucumber/features/RunCucumberTest.java
+++ src/test/java/milhagem/cucumber/features/RunCucumberTest.java
@@ -1,5 +1,8 @@
 package milhagem.cucumber.features;
 
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
@@ -7,4 +10,36 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/milhagem/cucumber/features", glue = "milhagem.cucumber", publish = true)
 public class RunCucumberTest {
+
+  @Dado("Horario é {int}:{int}")
+  public void horario_é(Integer int1, Integer int2) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+  @Dado("Cliente realiza um pedido no valor de R$ {double}")
+  public void cliente_realiza_um_pedido_no_valor_de_r$(Double double1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+  @Dado("Cliente irá buscar pessoalmente o pedido")
+  public void cliente_irá_buscar_pessoalmente_o_pedido() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+  @Quando("Cliente enviar o pedido")
+  public void cliente_enviar_o_pedido() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+  @Então("É definido a hora para buscar {int}:{int}")
+  public void é_definido_a_hora_para_buscar(Integer int1, Integer int2) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+  @Então("É cobrado o valor de R$ {double} ao cliente")
+  public void é_cobrado_o_valor_de_r$_ao_cliente(Double double1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
 }
--- src/test/java/milhagem/cucumber/features/milhagem.feature
+++ src/test/java/milhagem/cucumber/features/milhagem.feature
@@ -1,5 +1,12 @@
 # language: pt
-Funcionalidade: todo
+Funcionalidade: Pagamento do pedido
 
-  Cenario: Test
+  Cenario: Pagamento pessoalmente pelo cliente
+
+    Dado Horario é 11:00
+    E Cliente realiza um pedido no valor de R$ 40,00
+    E Cliente irá buscar pessoalmente o pedido
+    Quando Cliente enviar o pedido
+    Então É definido a hora para buscar 11:20
+    E É cobrado o valor de R$ 40,00 ao cliente
 
